e51b4fa0780fc0763d813f8a3bacf35758bdbc54
Allen R. Sudbring <allen@sudbring.com>
2019-03-23 23:25:30 -0500

    Fix comments

--- CustomSyncRule.ps1
+++ CustomSyncRule.ps1
@@ -6,14 +6,14 @@ If 'customAttribue1' contains BlockSync AND NotContain AllowSync then DO NOT SYN
 
 #>
 
-### Enter Variables for commands ###
+# Enter Variables for commands #
 $RuleName = 'Custom Rule Name'
 $ID = New-Guid
 $Description = 'Custom Rule Description'
 $Connector = Get-ADSyncConnector
 $attribute = 'customAttribute1'
 
-## This creates the new rule and names it and declares what object it's working on, in this case it's user/person so the rule will only work on user objects and puts the information in the variable $syncrule ##
+# This creates the new rule and names it and declares what object it's working on, in this case it's user/person so the rule will only work on user objects and puts the information in the variable $syncrule #
 New-ADSyncRule `
 -Name $RuleName `
 -Identifier $ID `
@@ -30,7 +30,7 @@ New-ADSyncRule `
 -ImmutableTag '' `
 -OutVariable syncRule
 
-## This adds the sync rule flow mapping to the variable
+# This adds the sync rule flow mapping to the variable #
 Add-ADSyncAttributeFlowMapping `
 -SynchronizationRule $syncRule[0] `
 -Source @('True') `
@@ -39,26 +39,26 @@ Add-ADSyncAttributeFlowMapping `
 -ValueMergeType 'Update' `
 -OutVariable syncRule
 
-## This adds the first rule that is firing off the "customAttribute1" and has "BlockSync" for the value and puts it in variable $condition0
+# This adds the first rule that is firing off the "customAttribute1" and has "BlockSync" for the value and puts it in variable $condition0 #
 New-Object `
 -TypeName 'Microsoft.IdentityManagement.PowerShell.ObjectModel.ScopeCondition' `
 -ArgumentList $attribute,'BlockSync','CONTAINS' `
 -OutVariable condition0
 
-## This adds the second rule that is firing off the "customAttribute1" and has either "AllowSync" or "NOTCONTAINS" and puts it in the variable #condition1
+# This adds the second rule that is firing off the "customAttribute1" and has either "AllowSync" or "NOTCONTAINS" and puts it in the variable #condition1 #
 New-Object `
 -TypeName 'Microsoft.IdentityManagement.PowerShell.ObjectModel.ScopeCondition' `
 -ArgumentList $attribute,'AllowSync','NOTCONTAINS' `
 -OutVariable condition1
 
-## This builds the rule and the condtions to the rule and puts it in variable #syncrule
+# This builds the rule and the condtions to the rule and puts it in variable #syncrule #
 Add-ADSyncScopeConditionGroup `
 -SynchronizationRule $syncRule[0] `
 -ScopeConditions @($condition0[0],$condition1[0]) `
 -OutVariable syncRule
 
-## Adds the Rule
+# Adds the Rule #
 Add-ADSyncRule -SynchronizationRule $syncRule[0]
 
-## Retrieves the setting of the rule
+# Retrieves the setting of the rule #
 Get-ADSyncRule -Identifier $ID
